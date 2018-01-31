8065b76e5d12948b04501a65386b0c84a8d44b44
nikybiasion <nicola.gorghetto@gmail.com>
2018-01-31 22:46:49 +0100

    second test

--- udbtest.py
+++ udbtest.py
@@ -1,12 +1,31 @@
 import socket
+import _thread
+import tkinter
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-MESSAGE = "Hello, World!"
+UDP_IP = "192.168.4.1"
+UDP_PORT = 9000
+MSG = ""
 
-print("UDP target IP:", UDP_IP)
-print("UDP target port:", UDP_PORT)
-print("message:", MESSAGE)
-
+global sock
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(bytes(MESSAGE, 'utf-8'), (UDP_IP, UDP_PORT))
+
+def udplistener():
+    while 1:
+        print("attesa")
+        data, socketsrc = sock.recvfrom(1024)
+        print(data.decode())
+
+def udpsender():
+    while 1:
+        data = input('comando')
+        print("invio %s" % data)
+        sock.sendto(bytes('%s\n' % data, 'utf-8'), (UDP_IP, UDP_PORT))
+
+try:
+    _thread.start_new_thread(udplistener, ())
+    _thread.start_new_thread(udpsender, ())
+except:
+    print("Error: unable to start thread")
+
+while 1:
+    pass
